f1d4e9f0bed5222fefd45d6b1cef1a966c09c21e
shahil-pixel <71595004+shahil-pixel@users.noreply.github.com>
2021-06-09 18:02:12 +0530

    Add files via upload

---
+++ components/MyHeader.js
@@ -0,0 +1,55 @@
+import React, { Component} from 'react';
+import { Header,Icon,Badge } from 'react-native-elements';
+import { View, Text, StyeSheet ,Alert} from 'react-native';
+import { render } from 'react-dom';
+
+export default class MyHeader extends Component{
+      constructor(props){
+        super(props);
+
+        this.state={
+          value:"",
+
+        }
+      }
+
+      getNumberOfUnreadNotifications(){ 
+        db.collection('all_notifications').where('notification_status','==',"unread")
+        .onSnapshot((snapshot)=>{ 
+          var unreadNotifications = snapshot.docs.map((doc) => doc.data()) 
+          this.setState({ 
+            value: unreadNotifications.length 
+          }) 
+        }) 
+      }
+
+      componentDidMount(){
+        this.getNumberOfUnreadNotifications();
+      }
+
+      BellIconWithBadge=()=>{
+        return(
+          <View>
+            <Icon name='bell' type='font-awesome' color='#696969' size={25}
+              onPress={() =>props.navigation.navigate('Notification')}/>
+             <Badge
+              value={this.state.value}
+             containerStyle={{ position: 'absolute', top: -4, right: -4 }}/>
+          </View>
+        )
+      }
+
+render(){
+  return (
+    <Header
+      leftComponent={<Icon name='bars' type='font-awesome' color='#696969'  onPress={() => props.navigation.toggleDrawer()}/>}
+      centerComponent={{ text: props.title, style: { color: '#90A5A9', fontSize:20,fontWeight:"bold", } }}
+      rightComponent={<Icon name ='bell' type ='font-awesome' color ='yellow' size ={25}
+      onPress={()=>props.navigation.navigate('Notification')}/>}
+      backgroundColor = "#eaf8fe"
+    />
+  );
+ };
+}
+
+export default MyHeader;
